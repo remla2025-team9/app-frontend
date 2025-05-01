6f1dd91bae1b1a185c96b6954d11bbe5c083e2f3
Timo van Hoorn <T.F.R.vanhoorn@student.tudelft.nl>
2025-05-01 10:52:18 +0200

    Added css and initialized shadcnui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <></>
+    <div className="max-w-screen-lg mx-auto w-full px-4 sm:px-6 my-12">
+      <h1 className="text-display-sm">Restaurant App</h1>
+    </div>
   );
 }
